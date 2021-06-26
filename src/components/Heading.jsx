38b1bdbb37c780ef6fe7f98534a7caf5e4c7451f
AMAL T S <amaltsthekkel@gmail.com>
2021-06-26 21:46:51 +0530

    Initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,4 @@
+function Heading() {
+  return <h1 className="hello">HELLO</h1>;
+}
+export default Heading;
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>first</li>
+      <li>secod</li>
+      <li>third</li>
+    </ul>
+  );
+}
+export default List;
