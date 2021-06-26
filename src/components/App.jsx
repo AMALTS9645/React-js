b97291b16db34e64b431e4c3c46ed19a9ae30267
AMAL T S <amaltsthekkel@gmail.com>
2021-06-27 00:59:40 +0530

    7day bootcamp by ShapeAI completed project React.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./footer";
+import Info from "./Info";
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
     </div>
   );
 }
